a42bb35dd11676d2bfff8f22d96356b7db8c2164
kleanthis kyriazakis <47750350+kkyriazakis@users.noreply.github.com>
2020-04-13 15:47:00 +0300

    Main update

--- Part B/src/Main.java
+++ Part B/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Chromosome t = GenerateChromosome();
         boolean x = checkFeasibility(t);
 
-        System.out.println( x );
+        System.out.println("---- " + x + " Dat");
     }
 
     public static boolean checkFeasibility(Chromosome chromosome){
@@ -18,47 +18,32 @@ public class Main {
         int afternoonW1, afternoonW2;
         int nightW1, nightW2;
 
-        for(int j=0; j<7; j++){
+        for(int j=0; j<7; j++){     //CHECKING HARD CONSTRAINTS
             morningW1=0; afternoonW1=0; nightW1=0;
             morningW2=0; afternoonW2=0; nightW2=0;
             for (int i=0; i<30; i++){
-                switch (array[i][j]){
-                    case 0:
-                        break;
-                    case 1:
-                        morningW1++;
-                        break;
-                    case 2:
-                        afternoonW1++;
-                        break;
-                    case 3:
-                        nightW1++;
-                        break;
+                switch (array[i][j]){       //CHECKING FIRST WEEK
+                    case 0: break;
+                    case 1: morningW1++; break;
+                    case 2: afternoonW1++; break;
+                    case 3: nightW1++; break;
                     default:
                         System.out.println(array[i][j]+" is not an option!");
+                        return false;
+                }
+
+                switch (array[i][j+7]){     //CHECKING SECOND WEEK
+                    case 0: break;
+                    case 1: morningW2++; break;
+                    case 2: afternoonW2++; break;
+                    case 3: nightW2++; break;
+                    default:
+                        System.out.println(array[i][j+7]+" is not an option!");
                         return false;
                 }
             }
             if(!(morningW1==hConst[j][0] && afternoonW1==hConst[j][1] && nightW1==hConst[j][2])){
                 return false;
-            }
-            for (int i=0; i<30; i++){
-                switch (array[i][j+7]){
-                    case 0:
-                        break;
-                    case 1:
-                        morningW2++;
-                        break;
-                    case 2:
-                        afternoonW2++;
-                        break;
-                    case 3:
-                        nightW2++;
-                        break;
-                    default:
-                        System.out.println(array[i][j+7]+" is not an option!");
-                        return false;
-                }
             }
             if(!(morningW2==hConst[j][0] && afternoonW2==hConst[j][1] && nightW2==hConst[j][2])){
                 return false;

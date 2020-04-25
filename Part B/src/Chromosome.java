4a2eed0ddb9373ef9067abc9ba527ef8b9b0e5de
kleanthis kyriazakis <47750350+kkyriazakis@users.noreply.github.com>
2020-04-25 13:20:27 +0300

    Chromosome update

--- Part B/src/Chromosome.java
+++ Part B/src/Chromosome.java
@@ -3,229 +3,129 @@ public class Chromosome {
     private int[][] state;
     private int score;
 
-    public Chromosome(int[][] population,int id) {
+    public Chromosome(int[][] population, int id) {
         this.state = population;
         this.score = 0;
         this.id = id;
     }
 
     public int getId() { return id; }
-
     public int[][] getState() { return state; }
-
     public void setState(int[][] state) { this.state = state; }
-
     public int getScore() { return score; }
 
-    public void updateScoreLow(){
-        this.score = 0;
-        int[] worker;
-        int tot_hours, cont_days, cont_nightShifts, prevShift;
-        int nShift,dShift,leave,weekends,prevOne,nextOne,cLeave;
-        boolean isWeekend, max_time, max_days, max_night, n2m , a2m, n2a, lwl, wlw, twoLafterfourN, twoLaftersevN, weekend_work ;
-
-        max_time = max_days = max_night = n2m = a2m = n2a = lwl = wlw = twoLafterfourN = twoLaftersevN = weekend_work = false;
-
-        for (int w=0; w<30; w++){    //FOR EACH WORKER
-            worker = this.state[w];
-            tot_hours = cont_days = cont_nightShifts = prevShift = 0;
-            nShift = dShift = leave = weekends = cLeave = 0; prevOne = nextOne = -1;
-            isWeekend = false;
-
-            for (int d=0; d<14; d++) {   //FOR EACH DAY
-
-                switch (worker[d]){ //UPDATE TOTAL HOURS
-                    case 1:
-                    case 2: tot_hours+=8; break;
-                    case 3: tot_hours+=10;break;
-                    case 0: break;
-                }
-
-                if ( worker[d] == 0 ){ //UPDATE AND CHECK CONT DAYS
-                    if( cont_days > 7 ){
-                        max_days = true;
-                    }
-                    cont_days = 0;
-                }
-                else
-                    cont_days++;
-
-                if ( worker[d] != 3 ){ //UPDATE AND CHECK CONT NIGHT SHIFTS
-                    if( cont_nightShifts > 4 ){
-                        max_night = true;
-                    }
-                    cont_nightShifts = 0;
-                }
-                else
-                    cont_nightShifts++;
-
-
-                if (prevShift == 3){ //CHECK LAST SHIFT
-                    if(worker[d] == 1){ n2m = true; } //NIGHT->MORNING
-                    else if(worker[d] == 2){ n2a = true; } //NIGHT->AFTERNOON
-                }
-                else if (prevShift == 2 && worker[d] == 1){ a2m = true; } //AFTERNOON->MORNING
-
-                prevShift = worker[d]; //UPDATE CURR SHIFT
-
-                if(d>=1) { prevOne=worker[d-1]; }
-                if(d<=12) { nextOne = worker[d + 1]; }
-                else{ nextOne=-1; }
-                isWeekend= d == 5 || d == 6 || d == 12 || d == 13;
-
-                switch (worker[d]){
-                    case 0: cLeave++; break;
-                    case 1:
-                    case 2:cLeave=0; dShift++;
-                        if(isWeekend) { weekends++; }
-                        break;
-                    case 3: cLeave=0; nShift++;
-                        if(isWeekend) { weekends++; }
-                        break;
-                }
-
-                //Scoring
-                if (nextOne != -1 && prevOne != -1){
-                    if (worker[d]==0 && prevOne!=0 && nextOne!=0) { wlw = true; }  // work-leave-work
-                    if (worker[d]!=0 && prevOne==0 && nextOne==0) { lwl = true; }  // leave-work-leave
-                }
-
-                if(cont_nightShifts == 4){      // At least 2 days of leave after 4 days of night shift in a row
-                    try{
-                        if(worker[d+1]!=0 || worker[d+2]!=0){ twoLafterfourN = true; }
-                    }catch (ArrayIndexOutOfBoundsException e){ twoLafterfourN = true; }
-                }
-                if(cont_days == 7){    // At least 2 days of leave after 7 days of work in a row
-                    try{
-                        if(worker[d+1]!=0 || worker[d+2]!=0){ twoLaftersevN = true; }
-                    }catch (ArrayIndexOutOfBoundsException e){ twoLaftersevN = true; }
-                }
-            }
-            //CHECK TOTAL HOURS
-            if(tot_hours > 70) { max_time = true; }
-
-            //Check for score
-            if(worker[5] != 0 || worker[6] != 0){ // At most one weekend (sat OR sun) of work
-                if (worker[12]!=0 || worker[13]!=0) { weekend_work = true; }
-            }
-        }
-
-        if (max_time) { this.score += 1000; }
-        if (max_days) { this.score += 1000; }
-        if (max_night) { this.score += 1000; }
-        if (n2m) { this.score += 1000; }
-        if (a2m) { this.score += 800; }
-        if (n2a) { this.score += 800; }
-        if (wlw) { this.score ++; }
-        if (lwl) { this.score ++; }
-        if (weekend_work) { this.score ++; }
-        if (twoLafterfourN) { this.score += 100; }
-        if (twoLaftersevN) { this.score += 100; }
-
-    }
-
-
-    public void updateScore(){
+    public void updateScore() {
         this.score = 0;
 
         int[] worker;
-        int tot_hours, cont_days, cont_nightShifts, prevShift, nShift,dShift,leave,weekends,prevOne,nextOne,cLeave;
+        int tot_hours, cont_days, cont_nightShifts, prevShift, nShift, dShift, leave, weekends, nextOne, cLeave;
         boolean isWeekend;
 
-        for (int w=0; w<30; w++){    //FOR EACH WORKER
+        for (int w = 0; w < 30; w++) {    //FOR EACH WORKER
             worker = this.state[w];
-            tot_hours = cont_days = cont_nightShifts = prevShift = 0;
-            nShift = dShift = leave = weekends = cLeave = 0;
-            prevOne = nextOne = -1;
+            tot_hours = cont_days = cont_nightShifts = 0;
+            nShift = dShift = weekends = cLeave = 0;
+            prevShift = -1;
 
-            for (int d=0; d<14; d++) {   //FOR EACH DAY
-                isWeekend= d == 5 || d == 6 || d == 12 || d == 13;
+            for (int d = 0; d < 14; d++) {   //FOR EACH DAY
+                if (d <= 12) { nextOne = worker[d + 1]; }
+                else { nextOne = -1; }
+
+                isWeekend = d == 5 || d == 6 || d == 12 || d == 13;
 
                 //UPDATE TOTAL HOURS
-                switch (worker[d]){
+                switch (worker[d]) {
                     case 1:
                     case 2: tot_hours += 8; break;
-                    case 3: tot_hours += 10;break;
+                    case 3: tot_hours += 10; break;
                     case 0: break;
                 }
 
                 //UPDATE AND CHECK CONT DAYS
-                if ( worker[d] == 0 ){
-                    if( cont_days > 7 ){
+                if (worker[d] == 0) {
+                    if (cont_days > 7) {
                         this.score += 1000;
                     }
                     cont_days = 0;
+                } else {
+                    cont_days++;
                 }
-                else { cont_days++; }
 
                 //UPDATE AND CHECK CONT NIGHT SHIFTS
-                if ( worker[d] != 3 ){
-                    if( cont_nightShifts > 4 ){
+                if (worker[d] != 3) {
+                    if (cont_nightShifts > 4) {
                         this.score += 1000;
                     }
                     cont_nightShifts = 0;
+                } else {
+                    cont_nightShifts++;
                 }
-                else { cont_nightShifts++; }
 
                 //CHECK LAST SHIFT
-                if (prevShift == 3){
-                    if(worker[d] == 1){ //NIGHT->MORNING
+                if (prevShift == 3) {
+                    if (worker[d] == 1) { //NIGHT->MORNING
                         this.score += 1000;
                     }
-                    else if(worker[d] == 2){ //NIGHT->AFTERNOON
+                    else if (worker[d] == 2) { //NIGHT->AFTERNOON
                         this.score += 800;
                     }
                 }
-                else if (prevShift == 2 && worker[d] == 1){ //AFTERNOON->MORNING
+                else if (prevShift == 2 && worker[d] == 1) { //AFTERNOON->MORNING
                     this.score += 800;
                 }
 
-                //UPDATE CURR SHIFT
-                prevShift = worker[d];
 
-                if(d>=1) { prevOne=worker[d-1]; }
-                if(d<=12) { nextOne = worker[d + 1]; }
-                else{ nextOne=-1; }
-
-                switch (worker[d]){
+                switch (worker[d]) {
                     case 0: cLeave++; break;
                     case 1:
-                    case 2: cLeave=0; dShift++;
-                            if(isWeekend)
+                    case 2: cLeave = 0;
+                            dShift++;
+                            if (isWeekend)
                                 weekends++;
                             break;
-                    case 3: cLeave=0; nShift++;
-                            if(isWeekend)
+                    case 3: cLeave = 0;
+                            nShift++;
+                            if (isWeekend)
                                 weekends++;
                             break;
                     default:
                 }
                 //Scoring
-                if (nextOne != -1 && prevOne != -1){
-                    if (worker[d]==0 && prevOne!=0 && nextOne!=0) score++;  // work-leave-work
-                    if (worker[d]!=0 && prevOne==0 && nextOne==0) score++;  // leave-work-leave
+                if (nextOne != -1 && prevShift != -1) {
+                    if ( prevShift != 0 && worker[d] == 0 && nextOne != 0) score++;  // work-leave-work
+                    if ( prevShift == 0 && worker[d] != 0 && nextOne == 0) score++;  // leave-work-leave
                 }
-                if(cont_nightShifts == 4){      // At least 2 days of leave after 4 days of night shift in a row
-                    try{
-                        if(worker[d+1]!=0 || worker[d+2]!=0){
-                            score = score+100;
+
+                if (cont_nightShifts == 4) {      // At least 2 days of leave after 4 days of night shift in a row
+                    try {
+                        if (worker[d + 1] != 0 || worker[d + 2] != 0) {
+                            score += 100;
                         }
-                    }catch (ArrayIndexOutOfBoundsException e){ score = score+100; }
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) { score += 100; }
                 }
-                if(cont_days == 7){    // At least 2 days of leave after 7 days of work in a row
-                    try{
-                        if(worker[d+1]!=0 || worker[d+2]!=0){
-                            score = score+100;
+                
+                if (cont_days == 7) {    // At least 2 days of leave after 7 days of work in a row
+                    try {
+                        if (worker[d + 1] != 0 || worker[d + 2] != 0) {
+                            score += 100;
                         }
-                    }catch (ArrayIndexOutOfBoundsException e){ score = score+100; }
+                    }
+                    catch (ArrayIndexOutOfBoundsException e) { score += 100; }
                 }
+
+                //UPDATE PREVIOUS SHIFT
+                prevShift = worker[d];
             }
+
             //CHECK TOTAL HOURS
-            if(tot_hours > 70) { this.score += 1000; }
+            if (tot_hours > 70) {
+                this.score += 1000;
+            }
 
             //Check for score
-            if(worker[5]!=0 || worker[6]!=0){
-                if (worker[12]!=0 || worker[13]!=0)
+            if (worker[5] != 0 || worker[6] != 0) {
+                if (worker[12] != 0 || worker[13] != 0)
                     score++; // At most one weekend (sat OR sun) of work
             }
         }
